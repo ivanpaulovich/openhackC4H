f1a7b7364bde93a03561b984de535733d4245229
ivanpaulovich <ivan@paulovich.net>
2018-12-01 11:26:55 +0100

    Initial files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Google.Cloud.Language.V1;
+
+namespace openhackC4H
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The text to analyze.
+            string text = "Software and system developers, gaming and digital media developers, system testers and test managers and system administrators.";
+            var client = LanguageServiceClient.Create();
+            var response = client.AnalyzeEntities(new Document()
+            {
+                Content = text,
+                Type = Document.Types.Type.PlainText
+            });
+        
+            Console.WriteLine($"Response: {response}");
+        }
+    }
+}
